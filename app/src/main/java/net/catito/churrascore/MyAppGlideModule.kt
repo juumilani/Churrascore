04eb948ec76989223dc312277e7d59688b48c870
juumilani <juumilani@hotmail.com>
2018-10-17 14:58:42 -0300

    acrescentadas as dependencias do glide

---
+++ app/src/main/java/net/catito/churrascore/MyAppGlideModule.kt
@@ -0,0 +1,7 @@
+package net.catito.churrascore
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class MyAppGlideModule : AppGlideModule ()
